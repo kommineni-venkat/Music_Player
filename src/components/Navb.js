352f668451bc0585a5d928a001f5c2c3fe6d4e64
kommineni-venkat <komminenivenkat101@gmail.com>
2024-06-07 15:36:23 +0530

    first commit

---
+++ src/components/Navb.js
@@ -0,0 +1,20 @@
+// FileName: Navb.js 
+
+import React from "react"; 
+
+const Nav = ({ setLibraryStatus, libraryStatus }) => { 
+	return ( 
+		<nav> 
+			<h1>Venky Music Player</h1> 
+			<button 
+				onClick={() => { 
+					setLibraryStatus(!libraryStatus); 
+				}} 
+			> 
+				<h4>PlayList</h4> 
+			</button> 
+		</nav> 
+	); 
+}; 
+
+export default Nav; 
